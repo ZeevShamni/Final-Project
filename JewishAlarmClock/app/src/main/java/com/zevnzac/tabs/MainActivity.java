504b0eab9ddb60ade3d0e037e326fbbd1710cbb0
Ze'ev <zeevshamni@gmail.com>
2018-12-19 15:47:26 +0200

    init commit

--- JewishAlarmClock/app/src/main/java/com/zevnzac/tabs/MainActivity.java
+++ JewishAlarmClock/app/src/main/java/com/zevnzac/tabs/MainActivity.java
@@ -24,7 +24,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     /**
-     * abcThe {@link android.support.v4.view.PagerAdapter} that will provide
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
